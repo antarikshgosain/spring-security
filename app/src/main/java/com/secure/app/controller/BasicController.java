227b7535dcdbb73af7c1e9caf859aeaea514d1fc
antarikshgosain <antariksh.gosain@gmail.com>
2024-11-16 14:24:09 -0500

    updated controller

--- app/src/main/java/com/secure/app/controller/BasicController.java
+++ app/src/main/java/com/secure/app/controller/BasicController.java
@@ -1,18 +1,19 @@
 package com.secure.app.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 @Slf4j
-public class TestController {
+public class BasicController {
 
-    @GetMapping(value = "/test")
-    public String testHello(){
+    @GetMapping(value = "/")
+    public String testHello(HttpServletRequest httpServletRequest){
         log.info("Endpoint Called - testHello()");
-        return "Project says Hello - Test Endpoint Working";
+        return "Project says Hello - Test Endpoint Working for " +httpServletRequest.getSession().getId();
     }
 }
