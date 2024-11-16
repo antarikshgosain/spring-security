0bdfec8e7327d8a18ec66d6e4672709bd0db0a27
antarikshgosain <antariksh.gosain@gmail.com>
2024-11-16 14:39:15 -0500

    In memory users added

---
+++ app/src/main/java/com/secure/app/security/InMemorySecurityConfig.java
@@ -0,0 +1,32 @@
+package com.secure.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class InMemorySecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("user")
+                .password(passwordEncoder().encode("user01"))
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin01"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
